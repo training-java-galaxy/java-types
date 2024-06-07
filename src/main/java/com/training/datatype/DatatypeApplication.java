9e86f94ce27f2a1005f0e7530d119f126c0ca76f
german.hernandez <inggermantics@gmail.com>
2024-06-06 22:03:51 -0600

    Init Commit

---
+++ src/main/java/com/training/datatype/DatatypeApplication.java
@@ -0,0 +1,13 @@
+package com.training.datatype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatatypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatatypeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/training/datatype/DatatypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.datatype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatatypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
